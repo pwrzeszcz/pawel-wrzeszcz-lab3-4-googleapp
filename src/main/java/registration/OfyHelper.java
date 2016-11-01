80ee8396d7bf121c092f984e039867bf5dcfd268
pavon147 <pawel20@10g.pl>
2016-11-01 22:35:30 +0100

    User entity

---
+++ src/main/java/registration/OfyHelper.java
@@ -0,0 +1,22 @@
+package registration;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextEvent;
+
+public class OfyHelper implements ServletContextListener
+{
+    public void contextInitialized(ServletContextEvent event)
+    {
+
+    }
+
+    public void contextDestroyed(ServletContextEvent event)
+    {
+
+    }
+}
+
---
+++ src/main/java/registration/User.java
@@ -0,0 +1,31 @@
+package registration;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Parent;
+
+import java.lang.String;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id public Long id;
+
+    public String name;
+    public String surname;
+    public String email;
+    public String password;
+
+    public User() {}
+    
+    public User(String name, String surname, String email, String password)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+}
